0202e5f0bca1585cebb70a59e8c1d3da3c354dda
sostene <ngarukiyimanasostene@gmail.com>
2022-05-11 22:38:29 +0200

    update our menu items to navigate with router HOC

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import HomePage from "./pages/homepage/homepage";
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Routes>
+        <Route path="/" exact element={<HomePage />} />
+        <Route path="hats" element={<HatsPage />} />
+        <Route path="sneakers" element={<Sneakers />} />
+      </Routes>
     </div>
   );
 }
 
 export default App;
+
+const HatsPage = () => {
+  return (
+    <div>
+      <h1>Hats Page</h1>
+    </div>
+  );
+};
+
+const Sneakers = () => {
+  return (
+    <div>
+      <h1>Sneakers Page</h1>
+    </div>
+  );
+};
--- src/components/menu-item/menu-item.jsx
+++ src/components/menu-item/menu-item.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import './menu-item.scss';
 
-const MenuItem = ({title,imageUrl,size}) => {
+const MenuItem = ({title,imageUrl,size,linkUrl}) => {
+    const navigate = useNavigate();
     return ( 
-        <div  className={`${size} menu-item`}>
+        <div  className={`${size} menu-item`} onClick={() => navigate(`${linkUrl}`) }>
             <div style={{ backgroundImage:`url(${imageUrl})`}} className="background-image" />
             <div className="content">
                 <h1 className='title'>{title.toUpperCase()}</h1>
