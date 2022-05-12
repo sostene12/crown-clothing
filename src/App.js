6e76948a9afae442b9def89c3ce0d4d79cef57f7
sostene <ngarukiyimanasostene@gmail.com>
2022-05-12 18:18:26 +0200

    adding the header component visible to all of our app components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import Header from "./components/header/header";
 import HomePage from "./pages/homepage/homepage";
 import ShopPage from "./pages/shop/shop";
 
 function App() {
   return (
     <div>
+      <Header />
       <Routes>
         <Route path="/" exact element={<HomePage />} />
         <Route path="shop" element={<ShopPage />} />
